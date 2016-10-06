de4fe6943a2de1e888e33526532ec81ea0e47d0b
jellical <14566778>
2016-10-07 02:15:21 +0700

    - No results message - Logout fixed

--- app/controllers/AppController.scala
+++ app/controllers/AppController.scala
@@ -3,6 +3,11 @@ package controllers
 import play.api.mvc.{Action, AnyContent, Controller}
 
 class AppController extends Controller {
+  def logout(): Action[AnyContent] = Action {
+    Redirect(routes.AppController.index())
+      .withNewSession
+  }
+
   def index(): Action[AnyContent] = Action {
     Ok(views.html.index.main())
   }
