9b5a67fb58f295ee3d7549aa1dc3c347bd7b576e
jellical <14566778>
2016-10-08 03:00:18 +0700

    - Removed restrictive condition

--- app/services/periodic/SchedulerActor.scala
+++ app/services/periodic/SchedulerActor.scala
@@ -58,7 +58,7 @@ class SchedulerActor @Inject() (socialServiceConnectors: SocialServiceConnectors
   }
 
   override def receive: Receive = {
-    case RequestPrivate.ExecuteNetworkUpdates if queue.nonEmpty && active.size < 5 =>
+    case RequestPrivate.ExecuteNetworkUpdates if queue.nonEmpty =>
       val record = queue.dequeue()
       active += record.person.id
 
