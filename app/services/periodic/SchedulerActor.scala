5ea443b58c0be162dda6a00b08cba052303fa2b2
Cyril A. Karpenko <self@nikelin.ru>
2016-10-12 07:02:51 +1300

    1 -> 5

--- app/services/periodic/SchedulerActor.scala
+++ app/services/periodic/SchedulerActor.scala
@@ -58,7 +58,7 @@ class SchedulerActor @Inject() (socialServiceConnectors: SocialServiceConnectors
   }
 
   override def receive: Receive = {
-    case RequestPrivate.ExecuteNetworkUpdates if queue.nonEmpty && active.size < 1 =>
+    case RequestPrivate.ExecuteNetworkUpdates if queue.nonEmpty && active.size < 5 =>
       val record = queue.dequeue()
       active += record.person.id
 
